429f698796c86681e728922f06e358c50c9311ee
Felipe <felipecattosilvabr@gmail.com>
2025-04-04 10:51:27 -0300

    Adicionando conversões

--- src/js/script.js
+++ src/js/script.js
@@ -1,14 +1,26 @@
-var nome = "Felipe"
-console.log(nome)
+// var nome = "Felipe"
+// console.log(nome)
 
-let idade = 18
-console.log(idade)
+// let idade = 18
+// console.log(idade)
 
-const sobrenome = "Silva"
-console.log(sobrenome)
+// const sobrenome = "Silva"
+// console.log(sobrenome)
 
-let exemplo
-console.log(exemplo)
+// let exemplo
+// console.log(exemplo)
 
-let ex1 = "Fiap"
-console.log(typeof ex1)
+// let ex1 = "Fiap"
+// console.log(typeof ex1)
+
+let numFloat = 123.456
+console.log(numFloat.toString())
+
+let numString = "123.123"
+console.log(parseFloat(numString))
+
+let numInt = 123456
+console.log(`${numInt}`)
+
+let numStri = "123456"
+console.log(Number(numStri))
